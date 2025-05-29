12d52b0f71360d099e3a450d972f70ead6fa68b3
Ardanny <ardannyromero@gmail.com>
2025-05-29 09:16:02 -0500

    Estudinte class casi completa

--- Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
+++ Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
@@ -4,10 +4,16 @@ public class Estudiante extends Persona {
     protected int creditos;
     protected String curso;
 
-    public Estudiante(String nombre, int edad, Genero genero) {
+    public Estudiante(String nombre, int edad, Genero genero, int creditos, String curso) {
         super(nombre, edad, genero);
         this.creditos = creditos;
         this.curso = curso;
+    }
+
+    @Override
+    public void mostrarPerfil() {
+        super.mostrarPerfil();
+        System.out.printf("\nCurso: %s\nCreditos: %d\n",curso ,creditos);
     }
 }
 
