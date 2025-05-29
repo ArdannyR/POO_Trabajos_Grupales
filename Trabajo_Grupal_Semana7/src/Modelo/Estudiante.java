83745dc400635a6458a458eba42cc6dde437a5b2
Ariel Macias <materchico@gmail.com>
2025-05-29 09:10:35 -0500

    Clase Persona

--- Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
+++ Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
@@ -4,9 +4,10 @@ public class Estudiante extends Persona {
     protected int creditos;
     protected String curso;
 
-    public Estudiante(String nombre, int edad, boolean genero) {
+    public Estudiante(String nombre, int edad, Genero genero) {
         super(nombre, edad, genero);
         this.creditos = creditos;
         this.curso = curso;
     }
 }
+
