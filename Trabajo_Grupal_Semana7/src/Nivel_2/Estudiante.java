57b9f810c2701bfdb568c9a9fc536282fe21722b
Ardanny <ardannyromero@gmail.com>
2025-05-29 08:46:02 -0500

    no changes

--- Trabajo_Grupal_Semana7/src/Nivel_2/Estudiante.java
+++ Trabajo_Grupal_Semana7/src/Nivel_2/Estudiante.java
@@ -4,4 +4,8 @@ import Nivel_1.Persona;
 
 public class Estudiante extends Persona {
 
+    public Estudiante(String nombre, int edad, Genero genero) {
+        super(nombre, edad, genero);
+
+    }
 }
