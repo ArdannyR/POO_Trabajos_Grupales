255c57df939060ddd2d6d5150b6f3610921078d3
Ariel Macias <materchico@gmail.com>
2025-05-29 09:28:23 -0500

    Clase Persona

--- Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
+++ Trabajo_Grupal_Semana7/src/Modelo/Estudiante.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 
 public class Estudiante extends Persona {
     protected String carrera;
+    protected int creditos;
     protected double promedio;
     protected ArrayList<String> cursosInscritos;
 
-    public Estudiante(String nombre, int edad, Genero genero, int creditos, String carrera) {
+    public Estudiante(String nombre, int edad, Genero genero, String carrera, int creditos, double promedio, ArrayList<String> cursosInscritos) {
         super(nombre, edad, genero);
         this.carrera = carrera;
+        this.creditos = creditos;
         this.promedio = promedio;
+        this.cursosInscritos = cursosInscritos;
     }
 
     @Override
