30f10a2a257a5efe94a1bd015811284a20f71a67
Ariel Macias <materchico@gmail.com>
2025-05-29 08:25:57 -0500

    Clase Persona

--- Trabajo_Grupal_Semana7/src/Nivel_1/Persona.java
+++ Trabajo_Grupal_Semana7/src/Nivel_1/Persona.java
@@ -1,4 +1,54 @@
 package Nivel_1;
 
 public class Persona {
+
+    // Definición del enum
+    enum Genero {
+        MASCULINO, FEMENINO
+    }
+
+    // Atributos de la superclase 'Persona'
+    private String nombre;
+    private int edad;
+    private Genero genero;
+
+    // Metodo constructor
+    public Persona(String nombre, int edad, Genero genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    // Metodo mostrarPerfil()
+    public void mostrarPerfil() {
+        System.out.println("\nInformación: ");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Género: " + genero);
+    }
+
+    // Metodos get y set
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
 }
--- Trabajo_Grupal_Semana7/src/Nivel_2/Estudiante.java
+++ Trabajo_Grupal_Semana7/src/Nivel_2/Estudiante.java
@@ -3,4 +3,6 @@ package Nivel_2;
 import Nivel_1.Persona;
 
 public class Estudiante extends Persona {
+
+
 }
